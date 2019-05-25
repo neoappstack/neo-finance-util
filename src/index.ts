8ad077fbaff4817c26817aa3fd46801df76a1173
Saurabh Verma <srvvrma0@gmail.com>
2019-05-25 15:46:34 +0530

    Setup Package

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export const Test = (name: string) => `Hello ${name}`;
---
+++ src/test/Greeter.test.ts
@@ -0,0 +1,5 @@
+import { Test } from '../index';
+
+test('My Greeter', () => {
+  expect(Test('Carl')).toBe('Hello Carl');
+});
